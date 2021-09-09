c6ca3776385a7bd0deac4682eafee85885e2c1d2
Your Namef <obiobialo@aol.com>
2021-09-09 12:22:34 -0500

    Added HTML and JS for number guessing game

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
-let randomNum = Math.floor((Math.random() * 100) + 1);
-let guess = 1;
+let randomNum = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+let output = document.getElementById("output-text");
 
-document.getElementById("btn").onclick = function() {
-    let x = document.getElementById("text").value
-
-    if (x === randomNum) {
-        alert("Nice job, you guessed the correct number!");
-    }
-    else if (x > randomNum) {
-        guess++
-        alert("Oops, the number is greater than the random number.");
-    }
-    else if (x < randomNum) {
-        guess++
-        alert("Oops, the number is less than the random number.");
-    }
-    else if (randomNum === x) {
-        alert("CONGRATS! You guessed the correct number");
-    }
-    if (guess = 3) {
-        alert("You ran out of attempts! The number is " + randomNum)
-    }
+function guessGame() {
+    let input = document.getElementById("number").value;
     
+    
+    if (input === randomNum) {
+        output.innerHTML = "Congrats! You guessed the correct number.";
+    }
+    else if (input > randomNum) {
+        output.innerHTML = "Sorry, you guessed to high";
+        attempts++
+    }
+    else if (input < randomNum) {
+        output.innerHTML = "Sorry, you guessed to low";
+        attempts++
+    }
+
+    
+
 }
 
-console.log("hello")
+  
